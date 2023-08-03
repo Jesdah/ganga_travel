2c0779a582b8e7399d2e73d0bc4a8d60e651f128
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-08-03 08:53:53 +0000

    commit for mentor review

--- blog/forms.py
+++ blog/forms.py
@@ -10,7 +10,7 @@ class AdventureForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model= Post
-        fields = ('title', 'content', 'author',)
+        fields = ('title', 'content',)
 
 class CommentForm(forms.ModelForm):
     class Meta:
