7c01c71b5b7bfe55f4c11520848b0463b9187cb0
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-08-18 20:29:51 +0000

    add visual effects to images in post and index.html

--- static/js/script.js
+++ static/js/script.js
@@ -24,3 +24,17 @@ window.onclick = function(event) {  // When the user clicks anywhere outside of 
     postModal.style.display = "none";
   }
 };
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
