568b0120f006bb9cd88fc4cba8ae57c529dfdf60
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-08-21 20:26:50 +0000

    add tests for get posts and add posts

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,21 @@
 from django.test import TestCase
+from .models import Adventure, Post
 
 
 
-# Create your tests here.
+class TestViews(TestCase):
+
+    def test_adventure_list(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'template/index.html')
+
+    def test_get(self):
+        response = self.client.get('<int:adventure_id>/<author_id>/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'template/post.html')
+    
+    def test_add_post(self):
+        response = self.client.post('<int:adventure_id>/<author_id>/', {'title': 'Test Add Destination'},
+        {'content': 'Test Add Destination'},{'featured_image': 'image.jpg'})
+        self.assertRedirects(response, '<int:adventure_id>/<author_id>/')
